899ad67e699f474096a29b6984abc6779ddc8b75
Duc Toan <22120376@student.hcmus.edu.vn>
2024-06-23 15:13:43 +0700

    Time: 7 ms (24.60%) | Memory: 9.63 MB (93.35%)

---
+++ 942.DI-String-Match/main.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+vector<int> diStringMatch(string s) {
+    int n = s.size();
+    vector<int> res(n + 1);
+    int low = 0, high = n;
+
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'I') {
+            res[i] = low++;
+        }
+        else {
+            res[i] = high--;
+        }
+    }
+    res[n] = low;
+
+    return res;
+}
+
+void print(vector<int> res) {
+	for (int i = 0; i < res.size(); ++i) {
+		cout << res[i] << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+
+	string s = "IDID";
+	vector<int> res = diStringMatch(s);
+	print(res);
+
+	return 0;
+}
