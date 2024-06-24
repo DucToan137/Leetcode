7395a89a44485bd3b77600a4655cc3c4d0829fe1
Duc Toan <22120376@student.hcmus.edu.vn>
2024-06-24 22:28:19 +0700

    Time: 11 ms (36.57%) | Memory: 41.00 MB (96.27%)

---
+++ 3162.Find-the-Number-of-Good-Pairs-I/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+	int res = 0;
+
+	for(int i=0;i<nums1.size();++i) {
+		for (int j = 0; j < nums2.size(); ++j) {
+			if (nums1[i] % (nums2[j] * k) == 0) {
+				res++;
+			}
+		}
+	}
+
+	return res;
+}
+
+int main() {
+
+	return 0;
+}
