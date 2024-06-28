a578f1e602a17712f2f023e96fba08248f4ebff2
Duc Toan <22120376@student.hcmus.edu.vn>
2024-06-28 12:36:39 +0700

    update

--- 1672.Richest-Customer-Wealth/main.cpp
+++ 1672.Richest-Customer-Wealth/main.cpp
@@ -10,13 +10,12 @@ int maximumWealth(vector<vector<int>>& accounts) {
 		int sum = 0;
 		for (int j = 0; j < accounts[i].size(); ++j) {
 			sum += accounts[i][j];
-			
+
 		}
 		if (sum > max) {
 			max = sum;
 		}
 	}
-
 
 	return max;
 }
