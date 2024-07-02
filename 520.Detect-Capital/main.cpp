a7114667584293dbe035b2d20d27948342c84707
Duc Toan <22120376@student.hcmus.edu.vn>
2024-07-02 23:14:32 +0700

    Time: 0 ms (100%) | Memory: 7.60 MB (29.19%)

--- 520.Detect-Capital/main.cpp
+++ 520.Detect-Capital/main.cpp
@@ -12,6 +12,26 @@ bool detectCapitalUse(string word) {
 		}
 	}
 	return count == 0 || count == n || (count == 1 && isupper(word[0]));
+
+	// Solution 2
+	/*int n = word.size(), lowercase = 0, uppercase = 0;
+
+	if (n == 1) return true;
+
+	bool temp = 0;
+	if (word[0] >= 'A' && word[0] <= 'Z') temp = 1;
+
+	for (int i = 1; i < n; i++) {
+		if (word[i] >= 'A' && word[i] <= 'Z') {
+			uppercase++;
+			if (temp == 0 || lowercase > 0) return 0;
+		}
+		else {
+			if (uppercase > 0) return 0;
+			lowercase++;
+		}
+	}
+	return 1;*/
 }
 
 int main() {
