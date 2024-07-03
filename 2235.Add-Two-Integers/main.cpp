8653133cf7fa50f419e76d9d14506a719ebaf8c6
Duc Toan <22120376@student.hcmus.edu.vn>
2024-07-04 02:16:42 +0700

    Time: 3 ms (42.90%) | Memory: 7.27 MB (26.69%)

---
+++ 2235.Add-Two-Integers/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+
+int sum(int num1, int num2) {
+	return num1 + num2;
+}
+
+int main() {
+
+	int a = 10;
+	int b = 20;
+	cout << sum(a, b) << endl;
+
+	return 0;
+}
