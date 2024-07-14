950fee262d2b416e547f5b571c207b4352e8fb35
Duc Toan <22120376@student.hcmus.edu.vn>
2024-07-14 19:57:41 +0700

    Time: 3 ms (15.18%) | Memory: 7.24 MB (5.94%)

---
+++ 2520.Count-the-Digits-That-Divide-a-Number/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+
+int countDigits(int num) {
+	int temp = num;
+	int count = 0;
+
+	while (temp > 0) {
+		int digit = temp % 10;
+		if (digit != 0 && num % digit == 0) {
+			count++;
+		}
+		temp /= 10;
+	}
+
+	return count;
+}
+
+int main() {
+
+	int n = 121;
+	cout << countDigits(n) << endl;
+
+	return 0;
+}
