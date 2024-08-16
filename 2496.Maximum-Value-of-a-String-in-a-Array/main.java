69c5c3da6ff9cf17e77441e41ce99b96a91df73c
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-16 10:00:33 +0700

    Time: 1 ms (87.67%) | Memory: 40.96 MB (67.31%)

---
+++ 2496.Maximum-Value-of-a-String-in-a-Array/main.java
@@ -0,0 +1,29 @@
+public class main {
+    public int maximumValue(String[] strs) {
+        int result = Integer.MIN_VALUE;
+
+        for (String str : strs) {
+            boolean flag = true;
+            int n = str.length();
+
+            for (char c : str.toCharArray()) {
+                if (Character.isAlphabetic(c)) {
+                    result = Math.max(result, n);
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                int num = Integer.parseInt(str);
+                result = Math.max(result, num);
+            }
+        }
+
+        return result;
+    }
+
+    public static void demo(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
