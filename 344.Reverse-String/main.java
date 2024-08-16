26b9515ad49b5ec177887a8d1bb86a1b18c90029
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-16 10:11:36 +0700

    Time: 0 ms (100.00%) | Memory: 48.98 MB (48.29%)

---
+++ 344.Reverse-String/main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char ch = s[i];
+            s[i] = s[j];
+            s[j] = ch;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = { 'h', 'e', 'l', 'l', 'o' };
+        reverseString(s);
+
+        for (char c : s) {
+            System.out.print(c + " ");
+        }
+
+    }
+}
