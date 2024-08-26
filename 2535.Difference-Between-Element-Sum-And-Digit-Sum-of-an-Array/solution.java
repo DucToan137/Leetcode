a261ecab44ea4a09fa2548efbc6e3b3810be5fc1
Duc Toan <22120376@student.hcmus.edu.vn>
2024-08-26 10:37:51 +0700

    Time: 2 ms (97.84%) |

---
+++ 2535.Difference-Between-Element-Sum-And-Digit-Sum-of-an-Array/solution.java
@@ -0,0 +1,31 @@
+import static java.lang.Math.abs;
+
+class solution {
+
+    public int sumDigit(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public int differenceOfSum(int[] nums) {
+        int sumDigit = 0;
+        int sumArray = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sumDigit += sumDigit(nums[i]);
+            sumArray += nums[i];
+        }
+
+        return Math.abs(sumDigit - sumArray);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 123, 45, 67, 89 };
+        solution s = new solution();
+        System.out.println(s.differenceOfSum(nums));
+    }
+}
