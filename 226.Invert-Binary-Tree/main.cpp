8eb358b68dcfecd315810fcce6d09a1377b2958e
Duc Toan <22120376@student.hcmus.edu.vn>
2024-09-02 17:55:05 +0700

    Time: 0 ms (100.00%) | Memory: 11.90 MB (35.96%)

---
+++ 226.Invert-Binary-Tree/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+                                                       right(right) {}
+};
+
+void mirror(TreeNode *root)
+{
+    if (root == nullptr)
+        return;
+
+    mirror(root->left);
+    mirror(root->right);
+
+    swap(root->left, root->right);
+}
+
+TreeNode *invertTree(TreeNode *root)
+{
+    mirror(root);
+    return root;
+}
+
+int main()
+{
+
+    return 0;
+}
