2ae8ea08a0c873a5dad3ed8bf25fb60b76967b1a
Duc Toan <22120376@student.hcmus.edu.vn>
2024-09-08 14:28:35 +0700

    Time: 1 ms (100.00%) | Memory: 40

---
+++ 728.Self-Diving-Numbers/solution.java
@@ -0,0 +1,31 @@
+public class solution {
+    public boolean isSelfDividing(int num) {
+        int n = num;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit == 0 || num % digit != 0) {
+                return false;
+            }
+            n /= 10;
+        }
+        return true;
+    }
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        for (int num = left; num <= right; num++) {
+            if (isSelfDividing(num)) {
+                res.add(num);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        solution sol = new solution();
+        int left = 1;
+        int right = 22;
+        List<Integer> res = sol.selfDividingNumbers(left, right);
+        System.out.println(res);
+    }
+}
