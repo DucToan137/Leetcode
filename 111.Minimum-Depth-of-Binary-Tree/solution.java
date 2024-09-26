c78243f088b76cf3954d864da08b19387914a35c
Duc Toan <22120376@student.hcmus.edu.vn>
2024-09-26 17:57:16 +0700

    Time: 4 ms (78.40%) | Memory: 83.28 MB (41.32%)

---
+++ 111.Minimum-Depth-of-Binary-Tree/solution.java
@@ -0,0 +1,43 @@
+public class solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        solution s = new solution();
+        TreeNode root = s.new TreeNode(3);
+        root.left = s.new TreeNode(9);
+        root.right = s.new TreeNode(20);
+        root.right.left = s.new TreeNode(15);
+        System.err.println(s.minDepth(root));
+    }
+}
