c19c6b17ddf41107b34d9dfd678858733a4d29ba
Duc Toan <22120376@student.hcmus.edu.vn>
2024-10-27 21:39:22 +0700

    Time: 3 ms (94.15%) | Memory: 12.78 MB (64.62%)

---
+++ 237.Delete-Node-in-a-Linked-List/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+void deleteNode(ListNode *node)
+{
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
+
+int main()
+{
+
+    return 0;
+}
