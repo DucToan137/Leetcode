ce3e2afd8681ca643a1b999273c23d85c01ebd25
Duc Toan <22120376@student.hcmus.edu.vn>
2024-10-29 17:49:41 +0700

    Time: 1 ms (93.59%) | Memory: 42.80 MB (53.23%)

---
+++ 107.Binary-Tree-Level-Order-Traversal-II/solution.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                level.add(current.val);
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+            result.addFirst(level);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        solution s = new solution();
+        TreeNode root = s.new TreeNode(3);
+        root.left = s.new TreeNode(9);
+        root.right = s.new TreeNode(20);
+        root.right.left = s.new TreeNode(15);
+        root.right.right = s.new TreeNode(7);
+
+        List<List<Integer>> result = s.levelOrderBottom(root);
+        for (List<Integer> level : result) {
+            for (int val : level) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
