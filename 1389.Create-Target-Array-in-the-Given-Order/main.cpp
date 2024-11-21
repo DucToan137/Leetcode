504d756ef842ee166ec9db10d6b21ac68c0edd20
Duc Toan <22120376@student.hcmus.edu.vn>
2024-11-21 12:58:01 +0700

    Time: 0 ms (100.00%) | Memory: 10.38 MB (70.18%)

---
+++ 1389.Create-Target-Array-in-the-Given-Order/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> createTargetArray(vector<int> &nums, vector<int> &index)
+{
+    vector<int> target;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        target.insert(target.begin() + index[i], nums[i]);
+    }
+
+    return target;
+}
+
+int main()
+{
+
+    return 0;
+}
